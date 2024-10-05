a442ce9e00c03b70fb619966ae2665b65d9e07a8
Qian Qiao <qian.qiao@gmail.com>
2024-10-06 02:58:23 +0800

    Added color to commandline

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod build_info;
 
-use clap::{Parser, Subcommand};
+use clap::builder::styling::{AnsiColor, Styles};
+use clap::{ColorChoice, Parser, Subcommand};
 use clap_verbosity_flag::{InfoLevel, Verbosity};
 
 use crate::build_info::BuildInfo;
@@ -17,7 +18,14 @@ impl std::fmt::Display for TeemiaoError {
 
 /// Teemiao is a tool that I have created for no particular reason at all just yet.
 #[derive(Debug, Parser)]
-#[command(version, about, arg_required_else_help = true)]
+#[command(version, about, arg_required_else_help = true,
+    color = ColorChoice::Auto,
+    styles = Styles::styled()
+        .header(AnsiColor::Green.on_default().bold())
+        .usage(AnsiColor::Green.on_default().bold())
+        .literal(AnsiColor::Blue.on_default().bold())
+        .placeholder(AnsiColor::Cyan.on_default())
+)]
 struct Cli {
     #[command(flatten)]
     verbose: Verbosity<InfoLevel>,
