1c179afb65c2fa6b5a2884e8c33c77fe3689e54c
Qian Qiao <qian.qiao@gmail.com>
2024-10-08 02:25:36 +0800

    Added documentations

--- src/build_info/mod.rs
+++ src/build_info/mod.rs
@@ -1,12 +1,23 @@
+// Copyright 2024 Qian Qiao
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //! Build information related functionalities.
 
-use std::path::PathBuf;
-
-use clap::Args;
-
 use crate::TeemiaoError;
-
+use clap::Args;
 use serde::Serialize;
+use std::path::PathBuf;
 
 /// Generate build information.
 ///
@@ -15,21 +26,24 @@ use serde::Serialize;
 ///
 /// The metadata includes the build time and the current git revision of the
 /// code base.
+///
+/// In the longer term, we would like to support VCS(s) other than just git,
+/// but as of now, this isn't a priority.
 #[derive(Debug, Args)]
 #[command()]
 pub struct BuildInfoCommand {
     /// Output file
-    #[arg(short, long)]
+    #[arg(short, long, default_value = "./build_info.json", value_name = "FILE")]
     out: Option<PathBuf>,
 }
 
-/// Build information
+/// Build information data structure.
 #[derive(Serialize)]
 pub struct BuildInfo {
-    /// Revision
+    /// Revision is the current git revision of the code base.
     revision: String,
 
-    /// Build time
+    /// Build time is the timestamp of the build.
     build_time: i64,
 }
 
